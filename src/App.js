c91702086ed59a1df165370ab7ffc62a195cbe84
Suraj <122242670+Kiran9991@users.noreply.github.com>
2023-12-03 13:43:47 +0530

    Made app error proof

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [retry, setRetry] = useState(false);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    if(response) setIsLoading(false);
+    setError(null)
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
     const data = await response.json();
+
     const transformedMovies = data.results.map((movieData) => {
       return {
         id: movieData.episode_id,
@@ -20,17 +29,43 @@ function App() {
         releaseDate: movieData.release_date,
       };
     });
+
     setMovies(transformedMovies);
+    } catch(error) {
+      setRetry(true)
+      if(retry) {
+        setError('...Retrying')
+        setInterval(() => {
+          fetchMoviesHandler();
+          console.log('fetching movies')
+        },50)
+      }
+      setError(error.message)
+    }
+    setIsLoading(false);
   }
+
+  function cancelFethchMoviesHandler() {
+    setRetry(false);
+    console.log('canceled')
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if(movies.length > 0) content = <MoviesList movies={movies} />;
+
+  if(error) content = <p>{error}</p>
+
+  if(isLoading) content = <p>Loading...</p>
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={cancelFethchMoviesHandler}>Cancel</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
-        <h5>{isLoading && `Loading data....`}</h5>
+        {content}
       </section>
     </React.Fragment>
   );
