ab3aaea1ca0cd489c953453b1f9ba4477de73aa0
Suraj <122242670+Kiran9991@users.noreply.github.com>
2023-12-05 20:06:42 +0530

    added add and delete api method

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
-import Form from "./components/form/Form";
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = useCallback(async function () {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
-
+      const response = await fetch('https://react-fetch-api-project-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
-        throw new Error("Something went wrong!");
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          titles: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
+      const loadedMovies = [];
 
-      setMovies(transformedMovies);
+      for(const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -41,18 +42,46 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-fetch-api-project-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    const data = await response.json();
+    setMovies((prev) => [...prev, { id:data.name,...movie }]);
+  }
+
+  async function deleteMovieHandler(id) {
+    await fetch(`https://react-fetch-api-project-default-rtdb.firebaseio.com/movies/${id}.json`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    setMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== id));
+  }
+
   let content = <p>Found no movies.</p>;
 
-  if (movies.length > 0) content = <MoviesList movies={movies} />;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} onDelete={deleteMovieHandler}/>;
+  }
 
-  if (error) content = <p>{error}</p>;
+  if (error) {
+    content = <p>{error}</p>;
+  }
 
-  if (isLoading) content = <p>Loading...</p>;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <Form/>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,17 @@ import React from 'react';
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+
+  const deleteMovie = () => {
+    props.onDelete(props.id);
+  }
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button className={classes.movieBtn} onClick={deleteMovie}>Delete Movie</button>
     </li>
   );
 };
